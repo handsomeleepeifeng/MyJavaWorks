4824a17bdfa54ec6e4a50a60f41dd753244878fc
handsomeleepeifeng <37867695+handsomeleepeifeng@users.noreply.github.com>
2019-06-05 16:27:01 +0800

    提交了替换空格的代码

---
+++ src/ProFiveReplaceSpace.java
@@ -0,0 +1,7 @@
+public class ProFiveReplaceSpace {
+    public String ProFiveReplaceSpaceSolution(String input){
+        String Output;
+        Output = input.replace(" ","%20");
+        return Output;
+    }
+}
